bc6dacb2e746189c136619595463405e0c8372ab
Chad A. Carino <ccarino@sundialbrands.com>
2016-10-05 17:37:40 -0400

    require.js shims for non AMD libs.

---
+++ requirejs-config.js
@@ -0,0 +1,14 @@
+var config = {
+  	"paths": {
+	   "bootstrap": 'sundialbrands/theme-frontend-sheamoisture/js/bootstrap.min.js',
+	   "youtubepopup": 'sundialbrands/theme-frontend-sheamoisture/js/YouTubePopUp_jquery.js'
+   	},
+  	"shim": {
+     	"bootstrap": {
+     		"deps":["jquery"]
+     	}
+     	"youtubepopup": {
+     		"deps":["jquery"]
+     	}
+    }
+};

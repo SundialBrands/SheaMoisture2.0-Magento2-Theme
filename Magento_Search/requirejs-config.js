cf5f1cc111a409d53af82b22eec086b82d7597f5
Chad A. Carino <ccarino@sundialbrands.com>
2016-10-05 10:49:22 -0400

    Initial version

---
+++ Magento_Search/requirejs-config.js
@@ -0,0 +1,12 @@
+/**
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+var config = {
+    map: {
+        '*': {
+            quickSearch: 'Magento_Search/form-mini'
+        }
+    }
+};
---
+++ web/js/customHead.js
@@ -0,0 +1,24 @@
+
+require([
+        'jquery'
+    ], function($){
+      $(document).ready(function(){
+ 				if($('.nav-toggle').css('display') == 'none'){
+ 					$('.header.links li a.language').parent().after( "<br/>" );
+ 				} else {
+ 					$('.header.links br').remove()
+ 				}
+ 				var searchInput = document.getElementById('search');
+
+ 				$(searchInput).on('blur', function(){
+ 					if($('.nav-toggle').css('display') == 'none'){
+ 						if($(searchInput).val() != '' ) {
+ 							$(searchInput).css('width','100%');
+ 						} else {
+ 							$(searchInput).css('width','46px%');
+ 					}
+ 				}
+ 			});
+
+ 		});
+    });

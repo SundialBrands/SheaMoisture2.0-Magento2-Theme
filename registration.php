ce062a1884872667749781454f7ebea98cfbf7ab
Chad A. Carino <ccarino@sundialbrands.com>
2016-11-08 10:22:34 -0500

    fix registration path

--- registration.php
+++ registration.php
@@ -6,6 +6,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::THEME,
-    'frontend/Sundial/sheamoisture',
+    'frontend/Sundialbrands/sheamoisture',
     __DIR__
 );

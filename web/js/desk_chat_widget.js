52133e72de1648107ac3b1ec56f7c0bcb96266e5
Chad A. Carino <ccarino@sundialbrands.com>
2017-04-12 23:02:12 -0400

    update phone number in help widget and bump version. Prior commit restores motion point link

--- web/js/desk_chat_widget.js
+++ web/js/desk_chat_widget.js
@@ -16,7 +16,7 @@ window.onload = function() {
 	if(chat_button_container.length < 1 || jQuery('#live-chat-widget div.chat-button').length > 0 ) {
 		return;
 	}
-	var helpdiv = '<div class="help-links" style="display:none;"><div class="help-links-close">X</div><div class="help-header">Our Customer Support Team is available from 10AM-5PM EST. Contact us via phone or live chat now.</div><div class="help-link">Chat: <span class="live-chat">Click here to chat now.</span></div><div class="help-link">Phone: <span><a href="tel:18552025180">1-855-202-5180</a></span></div><div class="help-link"><a href="#" onclick="window.coBrowsingInstance &amp;&amp; coBrowsingInstance.showPopup();return false;">Share Screen</a>: <span>Please Chat or Call First</span></div></div>';
+	var helpdiv = '<div class="help-links" style="display:none;"><div class="help-links-close">X</div><div class="help-header">Our Customer Support Team is available from 10AM-5PM EST. Contact us via phone or live chat now.</div><div class="help-link">Chat: <span class="live-chat">Click here to chat now.</span></div><div class="help-link">Phone: <span><a href="tel:16318428800">(631)842-8800</a></span></div><div class="help-link"><a href="#" onclick="window.coBrowsingInstance &amp;&amp; coBrowsingInstance.showPopup();return false;">Share Screen</a>: <span>Please Chat or Call First</span></div></div>';
 	chat_button_container.append(helpdiv + '<div title="Available from 10AM-5PM EST" class="help-button">Get Help</div>');
 	
 	var chat_agents = {

63f8ea1dfbe51b90c9eb1ff494a9ebba80fb8b32
Soumik Karmakar <developer.soumik@gmail.com>
2017-09-12 17:15:34 +0530

    Search expand fix

--- web/js/customHead.js
+++ web/js/customHead.js
@@ -1,35 +1,45 @@
-
 require([
         'jquery'
     ], function($){
-      $(document).ready(function(){
- 				if($('.nav-toggle').css('display') == 'none'){
- 					$('.header.links li a.language').parent().after( "<br/>" );
- 				} else {
- 					$('.header.links br').remove()
- 				}
- 				var searchInput = document.getElementById('search');
+    $(document).ready(function(){
+        if($('.nav-toggle').css('display') == 'none'){
+            $('.header.links li a.language').parent().after( "<br/>" );
+        } else {
+            $('.header.links br').remove()
+        }
+        /*var searchInput = document.getElementById('search');
 
- 				$(searchInput).on('blur', function(){
- 					if($('.nav-toggle').css('display') == 'none'){
- 						if($(searchInput).val() != '' ) {
- 							$(searchInput).css('width','100%');
- 						} else {
- 							$(searchInput).css('width','46px');
- 					}
- 				}
- 			});
-			if($('.nav-toggle').css('display') == 'block'){
-                $('.page-wrapper').addClass('IContainer');
-			}
-			if($('#shopCollections').length > 0){
-				if (window.location.search.indexOf('product_collection') < 1) {
-					document.getElementById('shopCollections').style.display = "block";
-				}
-			}
-			if($('.header.links > li > a.langLink').length > 0){
-				var $ll = jQuery('.header.links > li > a.langLink').first().clone();
-				$('.ves-menu nav.navigation .ves-megamenu').append($ll);	
-			}
- 		});
+        $(searchInput).on('blur', function(){
+            if($('.nav-toggle').css('display') == 'none'){
+                if($(searchInput).val() != '' ) {
+                    $(searchInput).css('width','100%');
+                } else {
+                    $(searchInput).css('width','46px');
+                }
+            }
+        });*/
+      $('input#search').click(function() {
+            $('input#search').css('width','100%');
+        });
+        $('input#search').blur(function() {
+            if($('input#search').val().length>=1) {
+                $('input#search').css('width','100%');
+            }
+            else {
+                $('input#search').css('width','46px');
+            }
+        });
+        if($('.nav-toggle').css('display') == 'block'){
+            $('.page-wrapper').addClass('IContainer');
+        }
+        if($('#shopCollections').length > 0){
+            if (window.location.search.indexOf('product_collection') < 1) {
+                document.getElementById('shopCollections').style.display = "block";
+            }
+        }
+        if($('.header.links > li > a.langLink').length > 0){
+            var $ll = jQuery('.header.links > li > a.langLink').first().clone();
+            $('.ves-menu nav.navigation .ves-megamenu').append($ll);
+        }
     });
+});

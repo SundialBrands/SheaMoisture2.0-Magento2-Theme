8e3936aa82aa4ec254bd8ece1ebf8e1d4fc3fede
ITCINFOTECH\23702 <vinay.jacob@itcinfotech.com>
2018-03-14 21:25:46 +0530

    Changed theme name to SheamoistureCC

--- registration.php
+++ registration.php
@@ -6,6 +6,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::THEME,
-    'frontend/Sundialbrands/sheamoisture',
+    'frontend/Sundialbrands/sheamoisturecc',
     __DIR__
 );

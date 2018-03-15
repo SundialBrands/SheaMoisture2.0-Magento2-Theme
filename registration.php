3a57043664c5a28e4d2c898a0bc7c791e7808c6b
ITCINFOTECH\23702 <vinay.jacob@itcinfotech.com>
2018-03-15 12:10:17 +0530

    Revert theme name

--- registration.php
+++ registration.php
@@ -6,6 +6,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::THEME,
-    'frontend/Sundialbrands/sheamoisturecc',
+    'frontend/Sundialbrands/sheamoisture',
     __DIR__
 );
